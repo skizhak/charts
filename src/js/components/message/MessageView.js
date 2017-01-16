2d4afbf163d491c901121d0b1351ade6c4fc347f
Dmitra <adrian@dmitra.com>
2017-01-16 18:15:54 +0200

    es6 convert Message component and utilize d3 selectors inside

--- src/js/components/message/MessageView.js
+++ src/js/components/message/MessageView.js
@@ -2,27 +2,26 @@
  * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
 const _ = require('lodash')
-const d3 = require('d3')
 const ContrailChartsView = require('contrail-charts-view')
 const _template = require('./message.html')
 
-var MessageView = ContrailChartsView.extend({
-  type: 'message',
-  tagName: 'div',
-  className: 'coCharts-message-view',
+class MessageView extends ContrailChartsView {
+  get type () { return 'message' }
+  get tagName () { return 'div' }
+  get className () { return 'coCharts-message-view' }
 
-  initialize: function (options) {
-    ContrailChartsView.prototype.initialize.call(this, options)
+  constructor (options) {
+    super(options)
     this.render()
     this.listenTo(this._eventObject, 'message', this.show)
     this.listenTo(this._eventObject, 'clearMessage', this.clear)
-  },
+  }
 
-  show: function (data) {
+  show (data) {
     let msgObj = _.assignIn({
       componentId: 'default',
       action: 'update',  // 'new', 'once', 'update'. future: 'dismiss', 'block'
-      messages: []
+      messages: [],
     }, data)
     let template = this.config.get('template') || _template
 
@@ -39,21 +38,20 @@ var MessageView = ContrailChartsView.extend({
 
     this.$el.html(template(msgObj))
 
-    d3.selectAll('[data-action="once"')
+    this.d3.selectAll('[data-action="once"')
       .style('opacity', 1)
       .transition()
       .duration(5000)
       .style('opacity', 1e-06)
       .remove()
-  },
+  }
 
-  clear: function (componentId) {
-    var messageSelector = '.message-row[data-component-id="' + componentId + '"]'
+  clear (componentId) {
+    const messageSelector = `.message-row[data-component-id="${componentId}"]`
     this.$(messageSelector).fadeOut('fast', () => {
       this.$(messageSelector).remove()
     })
-  },
-
-})
+  }
+}
 
 module.exports = MessageView
