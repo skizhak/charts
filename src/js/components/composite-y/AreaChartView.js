3be18306818bc99748fc7fd7b796da557d81a728
Dmitra <adrian@dmitra.com>
2017-01-25 17:28:57 +0200

    fix tooltip rendering
    add example page to demonstrate tooltip and custom tooltip

--- src/js/components/composite-y/AreaChartView.js
+++ src/js/components/composite-y/AreaChartView.js
@@ -111,14 +111,14 @@ class AreaChartView extends XYChartSubView {
         top: offset.top + pos[1],
         left: offset.left + pos[0] - this.xScale.range()[0],
       }
-      this._actionman.fire('ShowTooltip', tooltipOffset, dataItem, d.accessor.tooltip)
+      this._actionman.fire('ShowComponent', d.accessor.tooltip, tooltipOffset, dataItem)
     }
-    d3.select(d3.event.currentTarget).classed('active', true)
+    el.classList.add('active')
   }
 
   _onMouseout (d, el) {
     if (this.config.get('tooltipEnabled')) {
-      this._actionman.fire('HideTooltip', d.accessor.tooltip)
+      this._actionman.fire('HideComponent', d.accessor.tooltip)
     }
     el.classList.remove('active')
   }
