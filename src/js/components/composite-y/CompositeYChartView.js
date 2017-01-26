c81277a5b0cc9c0b182ce4b257df5da0fac259c5
Dmitra <adrian@dmitra.com>
2017-01-26 16:17:29 +0200

    consider rendering of shapes at the edge of axis
    margin is calculated by the biggest shape

--- src/js/components/composite-y/CompositeYChartView.js
+++ src/js/components/composite-y/CompositeYChartView.js
@@ -195,9 +195,9 @@ class CompositeYChartView extends ContrailChartsView {
         }
       }
       if (!this.hasAxisParam(axisName, 'range')) {
-        if (['bottom', 'top'].indexOf(this.params.axis[axisName].position) >= 0) {
+        if (['bottom', 'top'].includes(this.params.axis[axisName].position)) {
           this.params.axis[axisName].range = this.params.xRange
-        } else if (['left', 'right'].indexOf(this.params.axis[axisName].position) >= 0) {
+        } else if (['left', 'right'].includes(this.params.axis[axisName].position)) {
           this.params.axis[axisName].range = this.params.yRange
         }
       }
@@ -224,9 +224,29 @@ class CompositeYChartView extends ContrailChartsView {
         }
       }
     })
+    this.adjustAxisMargin()
+
     // Now update the scales of the appropriate drawings.
     _.each(this._drawings, (drawing) => {
       drawing.params.axis = this.params.axis
+    })
+  }
+  /**
+   * shrink x and y axises range to have margin for displaying of shapes sticking out of scale
+   */
+  adjustAxisMargin () {
+    let sizeMargin = 0
+    const sizeAxises = _.filter(this.params.axis, (v, k) => k.match('size'))
+    _.each(sizeAxises, axis => {
+      // assume max shape extension out of scale range as of triangle's half edge
+      // TODO margin should be based on the biggest triangle in the visible dataset but not the whole data
+      const axisSizeMargin = Math.sqrt(this.params.axis.sizeAxis.range[1] / Math.sqrt(3))
+      if (axisSizeMargin > sizeMargin) sizeMargin = axisSizeMargin
+    })
+    const axises = _.filter(this.params.axis, (v) => v.position)
+    _.each(axises, axis => {
+      const axisMargin = ['left', 'right'].includes(axis.position) ? -sizeMargin : sizeMargin
+      axis.scale.range([axis.range[0] + axisMargin, axis.range[1] - axisMargin])
     })
   }
   /**
