0741122c634bfc34de027e8ccdb11730e94c75d8
Abhishek <singla@juniper.net>
2017-01-30 21:28:28 -0800

    Come radial demo charts: storage pools and disk status

---
+++ examples/demo/radial-chart/pool-usage/pools.js
@@ -0,0 +1,90 @@
+// Most basic chart.
+const pieData = [
+  {label: 'Volumes', value: 112704659},
+  {label: 'Images', value: 96853788},
+  {label: 'Internal', value: 943853788},
+  {label: 'Volumes HDD A', value: 130792673},
+  {label: 'Volumns HDD B', value: 1127576593},
+]
+
+function memFormatter (number) {
+  const bytePrefixes = ['B', 'KB', 'MB', 'GB', 'TB']
+  let bytes = parseInt(number * 1024)
+  let formattedBytes = '-'
+  _.each(bytePrefixes, (prefix, idx) => {
+    if (bytes < 1024) {
+      formattedBytes = bytes.toFixed(1) + ' ' + prefix
+      return false
+    } else {
+      if (idx === bytePrefixes.length - 1) {
+        formattedBytes = bytes.toFixed(1) + ' ' + prefix
+      } else {
+        bytes = bytes / 1024
+      }
+    }
+  })
+  return formattedBytes
+}
+
+function numberFormatter (number) {
+  return d3.format(',.0f')(number) // eslint-disable-line no-undef
+}
+function getLabel (serie) {
+  return serie.label
+}
+function getValue (serie) {
+  return serie.value
+}
+
+const chartConfig = {
+  container: '#chart',
+  components: [{
+    type: 'ControlPanel',
+    config: {
+      menu: [{
+        id: 'Refresh',
+      }],
+    }
+  }, {
+    id: 'pieChartId',
+    type: 'PieChart',
+    config: {
+      type: 'donut',
+      radius: 100,
+      colorScale: d3.scaleOrdinal().range([
+        '#4caf50',
+        '#a88add',
+        '#fcc100',
+        '#6887ff',
+        '#00bcd4',
+        '#2196f3']), // eslint-disable-line no-undef
+      serie: {
+        getValue: getValue,
+        getLabel: getLabel,
+        valueFormatter: memFormatter,
+      },
+      tooltip: 'tooltipId',
+    },
+  }, {
+    id: 'tooltipId',
+    type: 'Tooltip',
+    config: {
+      dataConfig: [
+        {
+          accessor: 'value',
+          labelFormatter: getLabel,
+          valueFormatter: memFormatter,
+        },
+      ],
+    },
+  }, {
+    type: 'LegendUniversal',
+    config: {
+      sourceComponent: 'pieChartId',
+    },
+  }
+  ]
+}
+const chartView = new coCharts.charts.RadialChartView()
+chartView.setConfig(chartConfig)
+chartView.setData(pieData)
