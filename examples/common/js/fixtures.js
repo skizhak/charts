b7f0025ee361b05446161bb015880c16c6ad5cd8
Dmitra <adrian@dmitra.com>
2017-02-02 18:27:15 +0200

    add linear data to fixture

--- examples/common/js/fixtures.js
+++ examples/common/js/fixtures.js
@@ -7,9 +7,11 @@ const simpleStatic = []
   const data = simpleStatic
   const length = 20
   for (let i = 1; i <= length; i++) {
+    // Repeating region
+    // if (i > Math.floor(length * 0.3) && i < Math.floor(length * 0.5)) data.push(data[i - 2])
     // Gap between data
     if (i > Math.floor(length * 0.5) && i < Math.floor(length * 0.7)) continue
-    data.push({
+    const datum = {
       t: 1475760930000 + 1000000 * i,
       // Integer
       x: i,
@@ -21,7 +23,12 @@ const simpleStatic = []
       randomN: Math.random() > 0.7 ? -Math.random() : Math.random(),
       random5: Math.random() * 5,
       random10: Math.random() * 10,
-    })
+    }
+    // Linear region of 'a'
+    if (i > Math.floor(length * 0.3) && i < Math.floor(length * 0.4)) {
+      datum.a = data[i - 2].a
+    }
+    data.push(datum)
   }
 }
 
