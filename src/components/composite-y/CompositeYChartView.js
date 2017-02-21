77424edaab9c3c4711c5f79a6c138899b2419ca7
Sarin Kizhakkepurayil <sarink@juniper.net>
2017-02-21 15:27:24 -0800

    Fix removal of onResize from window.
    - we bind the super onResize to component view context. bind returns a
      new function reference. removeEventListener require this function
      signature to remove it properly. We will save the bound resize back to
      ._onResize and will use it for removal also.

--- src/components/composite-y/CompositeYChartView.js
+++ src/components/composite-y/CompositeYChartView.js
@@ -21,7 +21,12 @@ export default class CompositeYChartView extends ContrailChartsView {
 
     this.listenTo(this.model, 'change', this.render)
     this.listenTo(this.config, 'change', this._onConfigModelChange)
-    window.addEventListener('resize', this._onResize.bind(this))
+    /**
+     * Let's bind super _onResize to this. Also .bind returns new function ref.
+     * we need to store this for successful removal from window event
+     */
+    this._onResize = this._onResize.bind(this)
+    window.addEventListener('resize', this._onResize)
   }
 
   get tagName () { return 'g' }
