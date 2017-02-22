bc2ab3b85b88bca7df938329bbbce1725283e9c5
Sarin Kizhakkepurayil <sarink@juniper.net>
2017-02-22 10:16:36 -0800

    instance => view

--- examples/common/js/demo-loader.js
+++ examples/common/js/demo-loader.js
@@ -30,7 +30,7 @@ const templates = {
  * structure of an example:
  * 'example title': {
  *   template: 'template id', <= optional
- *   instance: chartObject, <= required
+ *   view: instance of chart view <= required
  *   description: {
  *     chartTitle: 'detailed chart title',
  *     chartDesc: 'chart descripiton'
@@ -40,14 +40,14 @@ const templates = {
 const allExamples = {
   'lineBar': {
     'Queries & DB R/W': {
-      instance: queryDBChart,
+      view: queryDBChart,
       description: {
         chartTitle: 'QE Queries on Analytics Node',
         chartDesc: 'Real-time Line vs Stacked Bar chart is used to compare queries and r/w requests to cassandra.'
       }
     },
     'Memory & CPU': {
-      instance: cpuMemChart,
+      view: cpuMemChart,
       description: {
         chartTitle: 'vRouter CPU vs Memory',
         chartDesc: 'Line vs Grouped Bar chart is used to compare CPU and Memory of vRouters.'
@@ -56,21 +56,21 @@ const allExamples = {
   },
   'bubble': {
     'Node CPU/Mem': {
-      instance: nodeCPUMemChart,
+      view: nodeCPUMemChart,
       description: {
         chartTitle: 'CPU & Memory of Contrail Nodes',
         chartDesc: 'Bubble chart with navigation is used to analyze CPU and Memory of different nodes. Each node is identified by it\'s temporary icon. Users can filter the nodes by CPU Share through navigation chart at bottom.'
       }
     },
     'Port Distribution': {
-      instance: portDistributionChart,
+      view: portDistributionChart,
       description: {
         chartTitle: 'VN Traffic In/Out across Ports',
         chartDesc: 'Top bubble charts displays the traffic in/out over a port range of a virtual network. Different, temporary icons are used to identify traffic in and traffic out. Users can filter the ports by traffic through navigation chart at bottom.'
       }
     },
     'vRouters': {
-      instance: vRoutersChart,
+      view: vRoutersChart,
       description: {
         chartTitle: 'VN, VMI, CPU, Memory of vRouters',
         chartDesc: 'Top bubble charts displays virtual networks and interfaces of a vRouter. Users can filter the vRouters by CPU share through navigation chart at bottom.'
@@ -80,7 +80,7 @@ const allExamples = {
   'grouped': {
     'Project VN Traffic': {
       template: 'grouped',
-      instance: vnDetailChart,
+      view: vnDetailChart,
       description: {
         chartTitle: 'Traffic Analysis for a Project',
         chartDesc: 'A combination of three charts used to analyze traffic across VNs under a project. LineBar chart shows either total traffic of this project or traffic of VN selected by clicking on Pie chart. Bubble chart shows traffic across ports with two icons: one for in-traffic and other one for out-traffic.'
@@ -88,7 +88,7 @@ const allExamples = {
     },
     'Compute Node': {
       template: 'grouped',
-      instance: computeNodeChart,
+      view: computeNodeChart,
       description: {
         chartTitle: 'Compute Node',
         chartDesc: 'A combination of five charts used to analyze these stats of a vRouter node: System/Node CPU/Memory, Flows, Process CPU/Memory, Disk Usages. Area chart shows System/Node memory usage. ' +
@@ -99,14 +99,14 @@ const allExamples = {
   },
   'area': {
     'VN Traffic In/Out': {
-      instance: inoutTrafficChart,
+      view: inoutTrafficChart,
       description: {
         chartTitle: 'Traffic in/out of two VNs',
         chartDesc: 'Real-time area chart is used to compare the in/out traffic of multiple VNs. Quadrant I displays traffic in and quadrant IV displays traffic out.'
       }
     },
     'vRouter Traffic': {
-      instance: vrTraffic,
+      view: vrTraffic,
       description: {
         chartTitle: 'Traffic of two VRs',
         chartDesc: 'Area chart is used to compare the total traffic of multiple vRouters.'
@@ -115,21 +115,21 @@ const allExamples = {
   },
   'radial': {
     'vRouter Traffic': {
-      instance: vRouterTrafficChart,
+      view: vRouterTrafficChart,
       description: {
         chartTitle: 'vRouter Traffic',
         chartDesc: 'A radial dendrogram used to show vRouter traffic between source and destination virtual-network, IP, port.'
       }
     },
     'OSD/Disk Status': {
-      instance: diskUsageChart,
+      view: diskUsageChart,
       description: {
         chartTitle: 'OSD status of Ceph cluster',
         chartDesc: 'Donut chart is used to show OSD states under a Ceph cluster.'
       }
     },
     'Storage Pools': {
-      instance: poolUsageChart,
+      view: poolUsageChart,
       description: {
         chartTitle: 'Pool allocation of Ceph cluster',
         chartDesc: 'Pie chart is used to show storage pool allocation under a Ceph cluster.'
@@ -143,32 +143,30 @@ const $exampleDesc = $('#exampleDesc') // anchor point for mounting the example 
 
 _.forEach(allExamples, (examples, chartCategory) => {
   let $links = $(`#${chartCategory}Links`)
-
   _.forEach(examples, (example, linkText) => {
-    var $link = createLink(chartCategory, example.template, example.instance, linkText, example.description)
-
+    var $link = createLink(chartCategory, example.template, example.view, linkText, example.description)
     $links.append($('<li>').append($link))
   })
 })
 
-function createLink (chartType = '', templateId = 'grouped', instance = {}, linkText = 'linkText', exampleDesc) {
+function createLink (chartType = '', templateId = 'grouped', view = {}, linkText = 'linkText', exampleDesc) {
   let cleaned = encodeURIComponent(linkText.replace(/\s/g, ''))
   let $link = $(`<a id="${chartType}${cleaned}" href="#${cleaned}"><span class="nav-text">${linkText}</span></a>`)
 
   $link.click((e) => {
-    let containerIds = _.isArray(instance.container) ? instance.container : [instance.container]
-    let currentInstance = $chartBox.data('currentInstance')
+    let containerIds = _.isArray(view.container) ? view.container : [view.container]
+    let currentView = $chartBox.data('chartView')
 
-    if (currentInstance) {
-      currentInstance.remove()
-      if (currentInstance.stopUpdating) {
-        currentInstance.stopUpdating()
+    if (currentView) {
+      currentView.remove()
+      if (currentView.stopUpdating) {
+        currentView.stopUpdating()
       }
     }
 
     $exampleDesc.empty()
     $chartBox.empty()
-    $chartBox.data('currentInstance', instance)
+    $chartBox.data('chartView', view)
 
     if (!_.isNil(exampleDesc)) {
       $exampleDesc.append(exampleDescTemplate({
@@ -178,12 +176,12 @@ function createLink (chartType = '', templateId = 'grouped', instance = {}, link
     }
 
     $chartBox.append(templates[templateId]({
-      groupedChartsWrapperId: instance.groupedChartsWrapper,
+      groupedChartsWrapperId: view.groupedChartsWrapper,
       containerIds: containerIds,
-      layoutMeta: instance.layoutMeta
+      layoutMeta: view.layoutMeta
     }))
 
-    instance.render()
+    view.render()
   })
 
   return $link
