44a0cb816881f19aa51582019615c2c8cc923fb6
Dmitra <adrian@dmitra.com>
2017-03-05 11:56:06 +0200

    examples loader now relies on chart's own template

--- examples/common/js/loader.js
+++ examples/common/js/loader.js
@@ -20,12 +20,6 @@ import shapes from '../../bubble-chart/multiple-shapes'
 import pieChart from '../../radial-chart/pie'
 import dendrogramChart from '../../radial-chart/dendrogram'
 import areaBasic from '../../area-chart/basic'
-import groupedChartTemplate from '../template/multiple.tmpl'
-
-const templates = {
-  grouped: groupedChartTemplate
-}
-
 import twoBarNav from '../../grouped-chart/linebar-linebar-nav/index.js'
 import twoLineBarOnePieNav from '../../grouped-chart/linebar-pie-nav/index.js'
 /**
@@ -88,75 +82,71 @@ const allExamples = {
   },
   'grouped': {
     '2 Bar Nav': {
-      template: 'grouped',
       view: twoBarNav
     },
     '2 LineBar 1 Pie Nav': {
-      template: 'grouped',
       view: twoLineBarOnePieNav
     }
   }
 }
 
+const $content = $('#content')
 const $chartBox = $('#chartBox')
 
 _.forEach(allExamples, (examples, chartCategory) => {
   let $links = $(`#${chartCategory}Links`)
   _.forEach(examples, (example, title) => {
-    var $link = createLink(chartCategory, example.template, example.view, title)
+    example.title = title
+    example.category = chartCategory
+    var $link = createLink(example)
     $links.append($('<li>').append($link))
   })
 })
 
-function _viewRenderInit (templateId, view) {
-  let containerIds = _.isArray(view.container) ? view.container : [view.container]
+function _viewRenderInit ({view, title = '', desc = ''}) {
   let currentView = $chartBox.data('chartView')
   if (currentView) {
     currentView.remove()
-    if (currentView.stopUpdating) {
-      currentView.stopUpdating()
-    }
+    if (currentView.stopUpdating) currentView.stopUpdating()
   }
-  // Cleanup and apply template.
+
+  // Cleanup and apply containers template
+  $content.find('#chartTitle').text(title)
+  $content.find('#chartDesc').text(desc)
   $chartBox.empty()
+  // set current view
   $chartBox.data('chartView', view)
-  $chartBox.append(templates[templateId]({
-    groupedChartsWrapperId: view.groupedChartsWrapper,
-    containerIds: containerIds,
-    layoutMeta: view.layoutMeta
-  }))
-  // Render it
   view.render()
 }
 
-function createLink (chartType = '', templateId = 'grouped', view, linkText) {
+function createLink (example) {
+  const chartType = example.category || ''
+  const view = example.view
   const RJSInitFlag = 'RJSInstantiated'
-  let cleaned = encodeURIComponent(linkText.replace(/\s/g, ''))
-  let $link = $(`<a id="${chartType}${cleaned}" href="#${chartType}${cleaned}"><span class="nav-text">${linkText}</span></a>`)
+  const cleaned = encodeURIComponent(example.title.replace(/\s/g, ''))
+  const link = `<a id="${chartType}${cleaned}" href="#${chartType}${cleaned}">
+    <span class="nav-text">${example.title}</span>
+    </a>`
+  const $link = $(link)
   if (view.type === 'RJS') {
-    $link.click((e) => {
-      if (view.status && view.status === RJSInitFlag) {
-        _viewRenderInit(templateId, view.AMDChartView)
-      } else {
-        // Load the entry point
-        let entryPoint = document.createElement('script')
-        entryPoint.src = 'node_modules/requirejs/require.js'
-        entryPoint.setAttribute('data-main', view.entryPoint)
-        document.body.append(entryPoint)
-        // Once the require entry point load is complete (not just the file load but all dependencies),
-        // the script callback will invoke render callback.
-        window.AMDRenderCB = (RJSChartView) => {
-          view.AMDChartView = RJSChartView
-          view.status = RJSInitFlag
-          _viewRenderInit(templateId, RJSChartView)
-        }
+    if (view.status && view.status === RJSInitFlag) {
+      example.view = view.AMDChartView
+    } else {
+      // Load the entry point
+      let entryPoint = document.createElement('script')
+      entryPoint.src = 'node_modules/requirejs/require.js'
+      entryPoint.setAttribute('data-main', view.entryPoint)
+      document.body.append(entryPoint)
+      // Once the require entry point load is complete (not just the file load but all dependencies),
+      // the script callback will invoke render callback.
+      window.AMDRenderCB = (RJSChartView) => {
+        view.AMDChartView = RJSChartView
+        view.status = RJSInitFlag
+        example.view = RJSChartView
       }
-    })
-  } else {
-    $link.click((e) => {
-      _viewRenderInit(templateId, view)
-    })
+    }
   }
+  $link.click(e => _viewRenderInit(example))
   return $link
 }
 const exampleId = window.location.hash || '#grouped2BarNav'
