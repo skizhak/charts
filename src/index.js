7738a080512d69a55b2ee121ce8ab1f046d31b1d
Dmitra <adrian@dmitra.com>
2017-03-10 18:29:26 +0200

    make predefined list of bubble shapes symbols available from cc lib

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
  */
 import * as providers from 'providers/index'
 import * as components from 'components/index'
+import * as Util from 'core/Util'
 import ChartView from 'core/ChartView'
-export {ChartView, components, providers}
+
+export {ChartView, components, providers, Util}
 
 import './styles/index.scss'
